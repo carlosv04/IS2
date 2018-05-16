4532562fd7681e31c20db65561f071aa3eb2a748
Angel Herrera <>
2018-05-16 13:45:45 -0500

    Se agrego los path a settings ya que causaban problemas al correrlo en Mac

--- settings.py
+++ settings.py
@@ -1,8 +1,16 @@
+import os
+
 # Configuraciones del juego
 TITLE = "Juan el Condor"
 WIDTH = 1000
 HEIGHT = 600
 FPS = 30
+
+# Path de los recursos
+current_path  = os.path.dirname(__file__) # Path de donde esta ubicado el settings.py
+imagenes_path = os.path.join(current_path, "imagenes") # Path de la carpeta "imagenes"
+fondo1_path   = os.path.join(imagenes_path, "FONDO1.png") # Path del fondo1
+suelo1_path   = os.path.join(imagenes_path, "suelo.png") # Path del suelo
 
 # Colores
 WHITE = (255, 255, 255)
